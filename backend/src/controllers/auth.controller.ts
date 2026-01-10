c50b53f760c0cd7b7513266ed34de4037f746039
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2026-01-10 15:58:44 +0530

    Added Login API

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import { generateToken } from "../utils/jwt";
+import AuthLog from "../models/AuthLog";
 
 // Register API
 export const register = async (req: Request, res: Response) => {
@@ -32,8 +34,10 @@ export const register = async (req: Request, res: Response) => {
     });
 
     return res.json({ message: "Registered successfully" });
-  } catch (err) {
-    return res.status(500).json({ message: "Server error" });
+  } catch (err: any) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: err.message });
   }
 };
 
@@ -44,13 +48,41 @@ export const login = async (req: Request, res: Response) => {
 
     const user = await User.findOne({ email });
 
-    // Login validations
     if (!user) {
       return res.status(400).json({ message: "Invalid email or password" });
     }
 
     const match = await bcrypt.compare(password, user.password_hash);
 
-    
-  } catch (error) {}
+    if (!match) {
+      return res.status(200).json({ message: "Invalid email or password" });
+    }
+
+    user.last_login_at = new Date();
+    const token = generateToken(user._id.toString(), user.role_id);
+
+    // Auth logs (commented out because of free database)
+    // await AuthLog.create({
+    //   user_id: user._id,
+    //   role_id: user.role_id,
+    //   event: "login",
+    //   status: "success",
+    // });
+
+    return res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.full_name,
+        email: user.email,
+        role_id: user.role_id,
+        role: user.role_id === 1 ? "Admin" : "Passenger",
+      },
+      message: "Login successfully",
+    });
+  } catch (err: any) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: err.message });
+  }
 };
