b542a5531ebccc9e57ab698f17946692f319e53f
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2026-01-10 19:32:07 +0530

    Done with user details API (Me)

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -9,15 +9,15 @@ export const register = async (req: Request, res: Response) => {
   try {
     const { full_name, email, phone, password } = req.body;
 
-    const email_exist = await User.findOne({ email });
-    const phone_exist = await User.findOne({ phone });
+    const emailExist = await User.findOne({ email });
+    const phoneExist = await User.findOne({ phone });
 
     // Registeration validations
-    if (email_exist) {
+    if (emailExist) {
       return res.status(200).json({ message: "Email already exists." });
     }
 
-    if (phone_exist) {
+    if (phoneExist) {
       return res.status(200).json({ message: "Phone number already exists." });
     }
 
@@ -79,6 +79,32 @@ export const login = async (req: Request, res: Response) => {
         role: user.role_id === 1 ? "Admin" : "Passenger",
       },
       message: "Login successfully",
+    });
+  } catch (err: any) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: err.message });
+  }
+};
+
+// User Details (Me) API
+export const me = async (req: any, res: any) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await User.findById(userId).select(
+      "_id full_name email phone role_id status is_verified created_at last_login_at"
+    );
+
+    if (!user) {
+      return res.status(200).json({ message: "User not found" });
+    }
+
+    return res.json({
+      data: {
+        ...user.toObject(),
+        role: user.role_id === 1 ? "Admin" : "Passenger",
+      },
     });
   } catch (err: any) {
     return res
