b806e3692831ac0f770543c3e3709337467de47b
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2026-01-11 02:37:52 +0530

    Backend cors handling done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes/auth.routes";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.use('/api/auth', router)
+app.use("/api/auth", router);
 
 export default app;
