e45914cfaf58ea6bb9b111f96e3cc98ed376117f
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2026-01-11 13:26:25 +0530

    Register page with proper design and using react hook form

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,8 +1,10 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginUser, registerUser } from "../api/auth.api";
+import type { AxiosError } from "axios";
+import type { RegisterPayload } from "../types/auth.types";
 
 export const useRegister = () => {
-  return useMutation({
+  return useMutation<void, AxiosError<{ message: string }>, RegisterPayload>({
     mutationFn: registerUser,
   });
 };
--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,7 +1,172 @@
+import { useForm } from "react-hook-form";
+import type { RegisterPayload } from "../../types/auth.types";
+import { useRegister } from "../../hooks/useAuth";
+import type { AxiosError } from "axios";
+import { User, Mail, Phone, Lock } from "lucide-react";
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<RegisterPayload>();
+
+  const { mutate, isPending } = useRegister();
+
+  const onSubmit = (data: RegisterPayload) => {
+    mutate(data, {
+      onError: (err: AxiosError<{ message: string }>) => {
+        setError("email", {
+          message: err.response?.data?.message || "Registration failed",
+        });
+      },
+    });
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
+      <div className="w-full max-w-md bg-white rounded-2xl shadow-lg shadow-purple-200 overflow-hidden border border-purple-200">
+        {/* Header Section */}
+        <div className="bg-purple-700 p-6 text-center text-white">
+          <h2 className="text-3xl font-extrabold">Create Account</h2>
+          <p className="text-purple-50 mt-2">Join us today and get started</p>
+        </div>
+
+        {/* Form Section */}
+        <form onSubmit={handleSubmit(onSubmit)} className="p-6 space-y-4">
+          {/* Full Name */}
+          <div className="space-y-1">
+            <label className="text-sm font-semibold text-gray-600 ml-1">
+              Full Name
+            </label>
+
+            <div className="relative mt-1">
+              <User
+                className="absolute left-3 top-3.5 text-purple-600"
+                size={19}
+              />
+              <input
+                {...register("fullName", { required: "Full name required" })}
+                placeholder="Karan Bolake"
+                className={`w-full pl-10 p-2.5 rounded-lg border outline-none transition-all duration-200 ${
+                  errors.fullName
+                    ? "border-red-500"
+                    : "border-gray-300 focus:border-purple-600 focus:ring-1 focus:ring-purple-300"
+                }`}
+              />
+            </div>
+
+            {errors.fullName && (
+              <p className="text-xs text-red-500 ml-1 mt-1 font-medium">
+                {errors.fullName.message}
+              </p>
+            )}
+          </div>
+
+          {/* Email */}
+          <div className="space-y-1">
+            <label className="text-sm font-semibold text-gray-600 ml-1">
+              Email
+            </label>
+
+            <div className="relative mt-1">
+              <Mail
+                className="absolute left-3 top-3.5 text-purple-600"
+                size={19}
+              />
+              <input
+                {...register("email", { required: "Email required" })}
+                placeholder="karan@test.com"
+                className={`w-full pl-10 p-2.5 rounded-lg border outline-none transition-all duration-200 ${
+                  errors.email
+                    ? "border-red-500"
+                    : "border-gray-300 focus:border-purple-600 focus:ring-1 focus:ring-purple-300"
+                }`}
+              />
+            </div>
+
+            {errors.email && (
+              <p className="text-xs text-red-500 ml-1 mt-1 font-medium">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+
+          {/* Phone */}
+          <div className="space-y-1">
+            <label className="text-sm font-semibold text-gray-600 ml-1">
+              Phone
+            </label>
+
+            <div className="relative mt-1">
+              <Phone
+                className="absolute left-3 top-3.5 text-purple-600"
+                size={18}
+              />
+              <input
+                // {...register("phone", { required: "Phone required" })}
+                placeholder="+91 9876543210"
+                className={`w-full pl-10 p-2.5 rounded-lg border outline-none transition-all duration-200 ${
+                  errors.phone
+                    ? "border-red-500"
+                    : "border-gray-300 focus:border-purple-600 focus:ring-1 focus:ring-purple-300"
+                }`}
+              />
+            </div>
+
+            {errors.phone && (
+              <p className="text-xs text-red-500 ml-1 mt-1 font-medium">
+                {errors.phone.message}
+              </p>
+            )}
+          </div>
+
+          {/* Password */}
+          <div className="space-y-1">
+            <label className="text-sm font-semibold text-gray-600 ml-1">
+              Password
+            </label>
+
+            <div className="relative mt-1">
+              <Lock
+                className="absolute left-3 top-3.5 text-purple-600"
+                size={19}
+              />
+              <input
+                type="password"
+                {...register("password", { required: "Password required" })}
+                placeholder="••••••••"
+                className={`w-full pl-10 p-2.5 rounded-lg border outline-none transition-all duration-200 ${
+                  errors.password
+                    ? "border-red-500"
+                    : "border-gray-300 focus:border-purple-600 focus:ring-1 focus:ring-purple-300"
+                }`}
+              />
+            </div>
+
+            {errors.password && (
+              <p className="text-xs text-red-500 ml-1 mt-1 font-medium">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+
+          <button
+            disabled={isPending}
+            className="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-3 rounded-xl shadow-md shadow-purple-200 transform transition-all active:scale-95 disabled:opacity-70 mt-4"
+          >
+            {isPending ? "Creating Account..." : "Create Account"}
+          </button>
+
+          <p className="text-center text-sm text-gray-500 mt-2">
+            Already have an account?{" "}
+            <span className="text-purple-600 font-semibold cursor-pointer hover:underline">
+              Log in
+            </span>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
